69a377aa2f8982183d6f9c371666013aa39994f0
sennepsild <magnusmhp@gmail.com>
2016-12-03 19:00:02 +0100

    last fix

---
+++ Assets/MenuScipts.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScipts : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene("HostJoin");
+    }
+    public void Rules()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
